a5e20f08dffdf302e248492769816fd0ccb6967b
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-12 11:23:16 +0000

    satck and heap

---
+++ 01_basics.js/04_stheap.js
@@ -0,0 +1,12 @@
+// stack(primitive),heap(non-primitive)
+//  for stack we only get the copy but for heap we get the refernce of the prevbious one
+// so when we change the value in stack we can change the copy value not the orginal one but in heap we change the whole value for the iniotial ojnme also
+
+let myutube="sameer"
+
+let another="mummy"
+another="someone"
+
+console.log(myutube);
+console.log(another);
+
