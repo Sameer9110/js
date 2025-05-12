7ef2ca7e392cd31c1baecf95fd5a5cca7559939e
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-12 09:16:52 +0000

    datatypes

---
+++ 01_basics.js/03_conop.js
@@ -0,0 +1,45 @@
+// let score="33"
+// console.log(typeof score);
+
+
+// let valueinnumber= Number(score)
+// console.log(typeof valueinnumber);
+
+//  "33"=33
+// "33abc"=Nan
+// 4true=1;false=0
+
+// let num=1
+// let boolaeannum=Boolean(num)
+// console.log(boolaeannum);
+
+//  1=true
+//  ""=false
+//  "hitesh"= true
+
+// same for number to string conversion usen String
+//  can also get negative of a vluen using   "-" this sign with the let
+//   summaarryyyyyy
+
+//  primitive
+// string,number,boolean,null,underfined,symbol,bigint
+
+  
+// refernece type
+// array,objects,functions
+
+const heros=["sameer","aryan","mummy"];
+let myobj={
+    name: "sameer",
+    aage: 20,
+}
+//  type of hero is object
+const myfunction= function(){
+    console.log("hello world");
+    
+}
+// type of myfunction is function
+console.log(typeof heros);
+
+
+
