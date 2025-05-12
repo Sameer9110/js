161db558a8b40f78b899bc09a380094008049a72
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-12 06:27:59 +0000

    datatypes

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,8 @@
+const accountid=12345
+let accountemail="sr@SVGMarkerElement"
+var accpassword="sameer"
+acccit="gaya"
+
+// prefer not to use var always use const or let bcz of issue in block scope and functionn scope
+
+ console.table([accountid, accountemail, accpassword])
---
+++ 01_basics.js/02_datatpes.js
@@ -0,0 +1,13 @@
+// alert(3+3) we are using node , not browsere
+// number = t to power of 53
+//  bigint
+//  string=""
+// bolean= true/false
+// null=standaalone value  type is object
+// undefined= type is undefined
+//  symbol=unique
+
+// object
+let name="sameer"
+
+console.log(typeof "sameer");
