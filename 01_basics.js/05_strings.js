48caa538cd5a33b8c4500e8c1735e2680074a326
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-13 18:51:00 +0000

    strings

---
+++ 01_basics.js/05_strings.js
@@ -0,0 +1,28 @@
+const name="sameer"
+const age=20
+
+console.log(name + age+"value");
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+const name2= new String('sameer')
+
+console.log(name2[0]);
+console.log(name2.__proto__);
+
+console.log(name2.length);
+console.log(name2.toUpperCase());
+console.log(name2.charAt(3));
+console.log(name2.indexOf('s'));
+const newname= name2.substring(0,4)
+console.log(newname);
+const newstring1="           sameer           "
+console.log(newstring1);
+console.log(newstring1.trim());
+
+
+
+const url="http://sameer.com/sameerisnice"
+console.log(url.replace('sameer', 'mummy'));
+console.log(url.includes('yuaeah'));
+
+console.log();
