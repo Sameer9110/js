6f9436383c566752f3a71595385a252b330064af
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-14 15:27:00 +0000

    nums and maths

---
+++ 01_basics.js/06_nums.js
@@ -0,0 +1,12 @@
+const score=30;
+const num= new Number(100)
+console.log(num);
+
+console.log(num.toString().length);
+console.log(num.toFixed(2));
+
+const number1= 123.667
+console.log(number1.toPrecision(3));
+
+
+console.log(Math.floor(Math.random()* (max-min+1)) +min);
