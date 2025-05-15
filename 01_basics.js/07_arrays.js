71a5a7642f0d6796ca98dc2989b556667ab8ee55
Sameer9110 <156506810+Sameer9110@users.noreply.github.com>
2025-05-15 10:37:14 +0000

    arrays

---
+++ 01_basics.js/07_arrays.js
@@ -0,0 +1,6 @@
+//   array methods
+
+// Array.unshift is used to add a number to the beginning of the array while push is used to add it to the end and shift is used to remove the first element while poip is used to remove the last element
+const newarr= MimeTypeArra.join()
+//  slice or splice
+//  slice is used as like if you do array.slice(1,3) then elemenst from 1 to 3 gets print excluding the 3rd index but when splice is doneit prints the last 3rd index and also changes the orginal array
